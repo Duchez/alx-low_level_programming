4e3f3be783a44afd6578bd3455834713b354ec14
Duchez <edukoo@gmail.com>
2020-09-16 20:58:19 +0300

    print format

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/**
+* main - Starting point
+*
+* Return: 0 (proof)
+*/
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
