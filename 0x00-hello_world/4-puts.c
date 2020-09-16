9939fb2dea2f39787cee792bbb13e8c0d959be4a
Duchez <edukoo@gmail.com>
2020-09-16 20:54:12 +0300

    print without printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Start Point
+*
+* Return: 0 (proof)
+*/
+
+int main(void)
+
+{
+
+char *s = "\"Programming is like building a multilingual puzzle";
+
+puts(s);
+
+return (0);
+}
