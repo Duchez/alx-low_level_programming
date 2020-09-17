5cb303916c0efac46e96a724e86eaa9e3524d9be
Duchez <edukoo@gmail.com>
2020-09-17 21:10:06 +0300

    count to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ *main- numbers 0-9
+ *
+ * Return: 0 (program ran)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i; i < 10; i++;)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
