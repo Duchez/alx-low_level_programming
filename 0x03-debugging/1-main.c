8afe3a4c0c74e17b9fedcd09c495251cf8823176
Duchez <edukoo@gmail.com>
2020-09-24 17:42:37 +0300

    infinite_looping

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - infinite loop
+*
+*Return: 0 Always
+**/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	/**
+	* i = 0;
+	*
+	* while (i < 10)
+	* {
+	* putchar(i);
+	* }
+	**/
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
