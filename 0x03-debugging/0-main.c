677ce3c91e9a35b9ce2f8fd7c928a71d02e7955a
Duchez <edukoo@gmail.com>
2020-09-24 17:41:58 +0300

    positive_or_negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* main - test positive_or_negative function
+*
+*Return: 0 succesful
+**/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
