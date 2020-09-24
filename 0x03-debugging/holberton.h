004964741bf6d417c80d4334e93f09f572a82295
Duchez <edukoo@gmail.com>
2020-09-24 17:45:48 +0300

    header file

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int x, int y, int z);
+void print_remaining_days(int mon, int day, int yr);
+int convert_day(int mon, int day);
+
+#endif
