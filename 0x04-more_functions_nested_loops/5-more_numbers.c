0ca27e78c678d1aa0a297c0d3aeae293bfef310e
Duchez <edukoo@gmail.com>
2020-09-25 16:37:56 +0300

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,19 @@
  * more_numbers - Print from 0 -14 ten times
  * @iter - iterator
  * @num - First iteration
- * @number_1 - Second Iteration
- * Return: Returns nothing since function is void
+ * @num1 - Second Iteration
+ * Return: void
  */
 
 void more_numbers(void)
 {
 	int iter;
-	int nums;
+	int num;
 	int num1;
 
 	for (iter = 1; iter <= 10; iter++)
 	{
-		for (numb = 0; num <= 14; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			num1 = num;
 			if ((num1 / 10) > 0)
