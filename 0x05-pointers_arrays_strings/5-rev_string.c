a9d2b06121ac22260d3202597f6d8f2509a4506a
Duchez <edukoo@gmail.com>
2020-09-29 17:03:43 +0300

    reverse di ting

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*rev_string - reverses a string
+*@s: string
+*
+*Return: void
+*/
+void rev_string(char *s)
+{
+	int f;
+	int b;
+	int c;
+
+	for (f = 0; s[f] != '\0'; f++)
+	{
+	}
+	f--;
+
+	for (b = 0; b < f; b++, f--)
+	{
+		c = s[f];
+		s[f] = s[b];
+		s[b] = c;
+	}
+}
