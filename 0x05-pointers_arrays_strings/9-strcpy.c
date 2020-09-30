36be3c448d091d003ea47f158307567656f6829c
Duchez <edukoo@gmail.com>
2020-09-30 17:25:46 +0300

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*_strcpy - copies a string
+*@dest: destination pointer
+*@src: source pointer
+*
+*Return: character pointer
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int iter;
+
+	for (iter = 0; src[iter] != '\0'; iter++)
+	{
+		dest[iter] = src[iter];
+	}
+	return (dest);
+}
