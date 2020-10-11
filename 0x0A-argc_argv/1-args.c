be02f3599cb72dd500d4b4858cb2c599fcab8072
Duchez <edukoo@gmail.com>
2020-10-11 18:56:43 +0300

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+*main - prints number of arguments passed in it
+*@argc: argument count
+*@argv: array pointer to arguments
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+}
