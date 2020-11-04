374c1bb2c9efe57afffe27678a67d9cb72431efa
Duchez <edukoo@gmail.com>
2020-11-04 18:53:17 +0300

    print elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements in listint_t
+ * @h: pointer to head
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	register size_t iter = 0;
+
+	while (h)
+	{
+		printf("&i\n", h->n);
+		h = h->next;
+		iter++;
+	}
+	return (iter);
+}
