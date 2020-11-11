f857d24648e1a6fdce488608dfcb0dae987c43f1
Duchez <edukoo@gmail.com>
2020-11-11 17:53:45 +0300

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: string to append to file
+ *
+ * Return: 1 if success, -1 if failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	register int filde;
+	register int write_bytes;
+	register int iter;
+
+	if (!filename)
+		return (-1);
+	filde = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (filde == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[iter])
+			iter++;
+		write_byte = write(filde, text_content, iter);
+		if (write_bytes == -1)
+			return (-1);
+	}
+	close(filde);
+	return (1);
+}
