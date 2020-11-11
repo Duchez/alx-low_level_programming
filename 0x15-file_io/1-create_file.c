22099760f94f9baa8ed1b30ff2b924b45aea347b
Duchez <edukoo@gmail.com>
2020-11-11 18:01:27 +0300

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[iter])
 			iter++;
-		write_byte = write(filde, text_content, iter);
+		write_bytes = write(filde, text_content, iter);
 		if (write_bytes == -1)
 			return (-1);
 	}
