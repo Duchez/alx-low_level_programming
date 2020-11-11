2afcef100c358975bc7873eece3b6b4c5eb70280
Duchez <edukoo@gmail.com>
2020-11-11 17:01:53 +0300

    desc and header

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads text from a file
+ * @filename: file name
+ * @letters: number of bytes
+ *
+ * Return: number of read bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filde;
+	ssize_t read_bytes;
+	char buffer[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	filde = open(filename, O_RDONLY);
+	if (filde == -1)
+		return (0);
+	read_bytes = read(filde, &buffer[0], letters);
+	read_bytes = write(STDOUT_FILENO, &buffer[0], read_bytes);
+	close(filde);
+	return (read_bytes);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+<h><strong>====== File IO======</strong></h>
+<p>This project tackles file input and output</p>
