5dbaf72e107cc659f37e576c0a5b2d028b1c70ee
Duchez <edukoo@gmail.com>
2020-11-21 18:04:33 +0300

    headers and description

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+<h><strong>====== Bit Manipulation ======</strong></h>
+<p>This Project explore bit manipulation in C.</p>
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned int m);
+int get_endianness(void);
+int _strlen(const char *s);
+_bool isbin(char);
+
+#endif
